b758be6547b302c124af9d2ffdf0aa6c4d465742
zhukyuri <zhukyuri@gmail.com>
2019-01-31 12:13:01 +0200

    Flow types

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ExampleComponent from './material/Example'
+import {Drag, Drop} from './material/DragDrop'
+
+export default ExampleComponent
+
+export {Drag, Drop}
---
+++ src/material/DragDrop/DragDropProvider.js
@@ -0,0 +1,204 @@
+// @flow
+
+import React, { Component } from 'react'
+
+type Props = {
+  cbDragStart?: any,
+  cbDragEnd?: any,
+  cbDragDrop?: any,
+  cbDragOver?: any,
+  cbDragEnter?: any,
+  cbDragLeave?: any,
+  cbDrop?: any,
+  cbDrag?: any,
+  children?: any,
+};
+
+type State = {}
+
+type typeDrag = {
+  dragid?: string,
+}
+
+class DragDropProvider extends Component<Props, State> {
+  constructor(props) {
+    super(props)
+
+    this.onDragStart = this.onDragStart.bind(this)
+    this.onDragEnd = this.onDragEnd.bind(this)
+
+    this.onDragOver = this.onDragOver.bind(this)
+    this.onDragEnter = this.onDragEnter.bind(this)
+    this.onDragLeave = this.onDragLeave.bind(this)
+    this.onDrop = this.onDrop.bind(this)
+    this.onDrag = this.onDrag.bind(this)
+
+    this.onAllEventDragDrop = this.onAllEventDragDrop.bind(this)
+
+    this.currentDragElementName = null
+    this.currentDropId = null
+  }
+
+  onAllEventDragDrop(method: string, elementName: string, e: SyntheticDragEvent<*>): void {
+    const {cbDragDrop} = this.props
+
+    if (cbDragDrop) {
+      cbDragDrop(method, elementName, e, this.currentDragElementName)
+    }
+
+  }
+
+  onDragStart(e: SyntheticDragEvent<HTMLElement>) {
+    const {cbDragStart} = this.props
+    const dataset: typeDrag = e.currentTarget.dataset
+    e.dataTransfer.effectAllowed = 'move'
+
+    if (dataset && dataset.dragid) {
+      this.currentDragElementName = dataset.dragid
+      this.currentMethod = 'DragStart'
+
+      if (cbDragStart) {
+        cbDragStart(this.currentMethod, this.currentDragElementName, e)
+      }
+
+      this.onAllEventDragDrop(this.currentMethod, this.currentDragElementName, e)
+    } else {
+      this.currentDragElementName = null
+    }
+  }
+
+  onDragEnd(e: SyntheticDragEvent<HTMLElement>) {
+    const {cbDragEnd} = this.props
+    const dataset: typeDrag = e.currentTarget.dataset
+
+    if (dataset && dataset.dragid) {
+      this.currentDragElementName = dataset.dragid
+      this.currentMethod = 'DragEnd'
+
+      if (cbDragEnd) {
+        cbDragEnd(this.currentMethod, this.currentDragElementName, e)
+      }
+
+      this.onAllEventDragDrop(this.currentMethod, this.currentDragElementName, e)
+    }
+
+    this.currentDragElementName = null
+    this.currentDropId = null
+  }
+
+  /**
+   * On Drag over
+   * @param {SyntheticDragEvent<*>} e
+   */
+  onDragOver(e: SyntheticDragEvent<*>) {
+    const {cbDragOver} = this.props
+    const dataset: typeDrag = e.currentTarget.dataset
+    e.preventDefault()
+
+    if (dataset && dataset.dragid) {
+      this.currentMethod = 'DragOver'
+
+      if (dataset.dragid && dataset.dragid !== this.currentDropId) {
+        if (cbDragOver) {
+          cbDragOver(this.currentMethod, this.currentDropId, e)
+        }
+
+        this.onAllEventDragDrop(this.currentMethod, dataset.dragid, e)
+      }
+
+      this.currentDropId = dataset.dragid
+    }
+  }
+
+  /**
+   * On Drag enter
+   * @param {SyntheticDragEvent<*>} e
+   */
+  onDragEnter(e: SyntheticDragEvent<*>) {
+    const {cbDragEnter} = this.props
+    const dataset: typeDrag = e.currentTarget.dataset
+    e.preventDefault()
+
+    if (dataset && dataset.dragid) {
+      this.currentMethod = 'DragEnter'
+      this.currentDropId = dataset.dragid
+
+      if (cbDragEnter) {
+        cbDragEnter(this.currentMethod, this.currentDropId, e)
+      }
+
+      this.onAllEventDragDrop(this.currentMethod, this.currentDropId, e)
+    }
+  }
+
+  /**
+   * On Drag leave
+   * @param {SyntheticDragEvent<*>} e
+   */
+  onDragLeave(e: SyntheticDragEvent<*>) {
+    const {cbDragLeave} = this.props
+    const dataset: typeDrag = e.currentTarget.dataset
+    e.preventDefault()
+
+    if (dataset && dataset.dragid) {
+      this.currentMethod = 'DragLeave'
+      this.currentDropId = dataset.dragid
+
+      if (cbDragLeave && dataset.dragid) {
+        cbDragLeave(this.currentMethod, this.currentDropId, e)
+      }
+
+      this.onAllEventDragDrop(this.currentMethod, this.currentDropId, e)
+    }
+  }
+
+  /**
+   * On Drop
+   * @param {SyntheticDragEvent<*>} e
+   */
+  onDrop(e: SyntheticDragEvent<*>) {
+    const {cbDrop} = this.props
+    const dataset: typeDrag = e.currentTarget.dataset
+
+    if (dataset && dataset.dragid) {
+      this.currentMethod = 'Drop'
+
+      if (cbDrop && dataset.dragid) {
+        cbDrop(this.currentMethod, this.currentDropId, e)
+      }
+
+      this.onAllEventDragDrop(this.currentMethod, dataset.dragid, e)
+    }
+  }
+
+  /**
+   * On Drag
+   * @param {SyntheticDragEvent<*>} e
+   */
+  onDrag(e: SyntheticDragEvent<*>) {
+    const {cbDrag} = this.props
+    const dataset: typeDrag = e.currentTarget.dataset
+
+    if (dataset && dataset.dragid) {
+      this.currentMethod = 'Drag'
+
+      if (cbDrag && dataset.dragid) {
+        cbDrag(this.currentMethod, this.currentDropId, e)
+      }
+
+      this.onAllEventDragDrop(this.currentMethod, dataset.dragid, e)
+    }
+  }
+
+  render() {
+    const {children} = this.props
+
+    return (
+      <DragDropContext.Provider>
+        {children}
+      </DragDropContext.Provider>
+    );
+  }
+}
+
+export default DragDropProvider;
